84509c7a096beaf52ddae23251373a8fb204cfab
Matt Styles <matt@veryfizzyjelly.com>
2016-01-11 10:46:46 +0000

    #add octaves and persistence to break up the simplex terrain

--- poc-perlin/index.js
+++ poc-perlin/index.js
@@ -3,6 +3,7 @@
 
 var fit = require( 'canvas-fit' )
 var Simplex = require( 'fast-simplex-noise' )
+var Noise = require( 'noisejs' ).Noise
 var Bezier = require( 'bezier-easing' )
 var ndarray = require( 'ndarray' )
 var PRNG = require( 'seedrandom' )  // @TODO massively adds to bundle size, use another
@@ -24,35 +25,68 @@ function gradient( origin, radius, target ) {
   return 1.0 - dist( origin, target ) / radius
 }
 
+var perlin = new Noise( .25 )
 
-const freq = 5.5
+var freq = 7.7
 
-// var noise = new Simplex({
+// Remember to add octaves, otherwise it defaults to 1 which is smooth
 window.noise = new Simplex({
-  min: 0,
+  min: -1,
   max: 1,
+  octaves: 8,
+  persistence: .55,
   frequency: 1 / Math.pow( 2, freq ),
   random: new PRNG( require( './package.json' ).name )
 })
 window.noise2 = new Simplex({
-  min: 0,
+  min: -1,
   max: 1,
-  frequency: 1 / Math.pow( 2, freq - 1 ),
-  random: new PRNG( require( './package.json' ).name  )
+  octaves: 8,
+  persistence: .65,
+  frequency: 1 / Math.pow( 2, freq ),
+  random: new PRNG( require( './package.json' ).name + 'roughness'  )
 })
-const JITTER_AMOUNT = .05
+const JITTER_AMOUNT = .1
 window.jitter = new Simplex({
   min: -1,
   max: 1,
-  frequency: 1 / Math.pow( 2, freq * .55 ),
-  random: new PRNG( require( './package.json' ).name )
+  octaves: 4,
+  frequency: 1 / Math.pow( 2, freq * .15 ),
+  random: new PRNG( require( './package.json' ).name + 'detail' )
 })
 
 // @TODO easing like this is slow
 // var ease = new Bezier( 0, 0, .75, 1 )
-window.ease = new Bezier( .25, 0, .65, 1 )
+// window.ease = new Bezier( .25, 0, .65, 1 )
+
+// 3x^2 - 2x^3
+// much much quicker than calculating the bezier
+window.ease = {
+  get: function( v ) {
+    return v*v*(3 - 2 * v )
+  }
+}
+
+// this produces good looking landmasses and is quicker than the simplex version above,
+// although that is probably just due to less octaves being calculated
+function terrain( x, y ) {
+  var h = perlin.perlin2( x, y )
+  x *= 2.94
+  y *= 2.94
+  h += perlin.perlin2( x, y ) * .4
+  x *= 2.87
+  y *= 2.87
+  h += perlin.perlin2( x, y ) * .15
+  x *= 2.63
+  y *= 2.63
+  h += perlin.perlin2( x, y ) * .075
+  // return Math.pow( h, 2 )
+  return h
+}
 
 var map = ndarray( [ DIMS[ 0 ] * DIMS[ 1 ] ], [ DIMS[ 0 ], DIMS[ 1 ] ] )
+
+var FREQ = 1 / 80
 
 var height = null
 var elevation = null
@@ -63,15 +97,24 @@ function generate() {
   var start = performance.now()
   for ( var y = 0; y < DIMS[ 1 ]; y++ ) {
     for ( var x = 0; x < DIMS[ 0 ]; x++ ) {
-      elevation = noise.get2DNoise( x, y )
-      roughness = noise2.get2DNoise( x, y )
-      detail = jitter.get2DNoise( x, y ) * JITTER_AMOUNT
-      // height = noise.get2DNoise( x, y )
-      // height *= noise2.get2DNoise( x, y ) * .5 + .5
-      // height = ( noise.get2DNoise( x, y ) * .65 ) + ( noise2.get2DNoise( x, y ) * .35 )
+      // elevation = noise.get2DNoise( x, y )
+      // roughness = noise2.get2DNoise( x, y )
+      // detail = jitter.get2DNoise( x, y ) * JITTER_AMOUNT
+      elevation = terrain( x * FREQ, y * FREQ )
+      roughness = terrain( x * FREQ * .95, y * FREQ * .95 )
+      detail = terrain( x * FREQ * 100, y * FREQ * 100 ) * JITTER_AMOUNT
+
       height = ( elevation + ( roughness * detail ) )
+      // height = elevation
+      // height = roughness * detail
+      // height = ease.get( height )
+
+      // height = perlin.perlin2( x * FREQ, y * FREQ )
+      // height = terrain( x * FREQ, y * FREQ )
+
+      height = .5 + .5 * height
       height = ease.get( height )
-      // height += gradient( center, 128, [ x, y ] ) * .65
+
       map.set( x, y, height )
     }
   }
@@ -85,6 +128,14 @@ function render() {
 function go() {
   generate()
   render()
+
+  var low = Number.MAX_SAFE_INTEGER
+  var high = Number.MIN_SAFE_INTEGER
+  map.data.forEach( h => {
+    low = h < low ? h : low
+    high = h > high ? h : high
+  })
+  console.log( 'low', low, 'high', high )
 }
 
 go()
--- poc-perlin/render.js
+++ poc-perlin/render.js
@@ -18,7 +18,22 @@ function color( value, alpha )  {
   return makeColor( color, alpha || 1 )
 }
 
-var BLOCK_SIZE = 4
+// Turns a nice curve into a stepped curve
+// Value is expected to be 0...1
+function step( value, divisions ) {
+  var d = 1 / divisions || 2
+  for( var i = 0; i < 1; i += d ) {
+    if ( value >= i && value < i + d ) {
+      return i
+    }
+  }
+  console.warn( 'step should never produce this', value )
+  return 0
+}
+
+var BLOCK_SIZE = 2
+var WATER_LEVEL = .5
+var HEIGHT_STEP = 20
 
 module.exports = function renderable( canvas ) {
   var ctx = canvas.getContext( '2d' )
@@ -29,24 +44,27 @@ module.exports = function renderable( canvas ) {
   function sample( map, x, y, min, max ) {
     let count = 0
     let range = ( max - min ) + 1
-    for ( var i = y + min; i < y + max; i++ ) {
-      for ( var j = x + min; j < x + max; j++ ) {
+    for ( var i = y + min; i <= y + max; i++ ) {
+      for ( var j = x + min; j <= x + max; j++ ) {
         count += map.get( i, j ) || 0
       }
     }
-    // return count / ( range * 2 )
-    return count / 9
+    return count / ( range * range )
   }
 
+  window.sample = sample
+
   return function render( map ) {
+    var start = performance.now()
     ctx.clearRect( 0, 0, map.shape[ 0 ] * BLOCK_SIZE, map.shape[ 1 ] * BLOCK_SIZE )
     for ( var y = 0; y < map.shape[ 1 ]; y++ ) {
       for ( var x = 0; x < map.shape[ 0 ]; x++ ) {
         height = map.get( x, y )
-        // height = sample( map, x, y, -1, 1 )
+        //height = sample( map, x, y, -1, 1 )
 
-
-        col = color( height < .35 ? height * .5 : height, 1 )
+        // height = step( height, HEIGHT_STEP )
+        // col = color( height < WATER_LEVEL ? 0 : 1, 1 )
+        col = color( height < WATER_LEVEL ? height * .5 : height, 1 )
         // col = color( height )
 
 
@@ -54,5 +72,6 @@ module.exports = function renderable( canvas ) {
         ctx.fillRect( x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE )
       }
     }
+    console.log( 'render done:', performance.now() - start )
   }
 }
